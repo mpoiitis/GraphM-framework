0c2bccd3f8af6ea5dd0e21631e270969cdb1c24a
Marinos Poiitis <marinospoet@gmail.com>
2020-08-26 16:06:06 +0300

    Add multiple dataset read methods

--- readData.py
+++ readData.py
@@ -1,5 +1,10 @@
 from time import time
 import networkx as nx
+import pandas as pd
+from scipy.io import loadmat
+from scipy.sparse import issparse
+import os
+import numpy as np
 
 def load_edgelist(file, directed=False, weighted=None):
     """
@@ -53,3 +58,97 @@ def load_adjacencylist(file, directed=False, weighted=None):
     print('Graph loaded in {}s'.format(t1 - t0))
 
     return G
+
+
+def load_matfile(file='data/Amherst41.mat', directed=False):
+    """
+    Load graph from .mat file. Column 5 - "year" - is used as user label.
+    :param file: the .mat file
+    :param directed: whether the graph should be directed
+    :return: the graph, x feature data, y labels
+    """
+
+    if not file.lower().endswith('.mat'):
+        raise Exception('Wrong file type is given. Should be *.mat')
+
+    t0 = time()
+    mat_variables = loadmat(file)
+    mat_matrix = mat_variables["A"]  # adjacency matrix
+
+    feat_matrix = mat_variables["local_info"]  # feature matrix for each node
+
+    df = pd.DataFrame(feat_matrix)
+    x = df.drop(5, axis=1)
+    y = df[5]
+
+    G = nx.DiGraph(weight=1)
+    if issparse(mat_matrix):
+        cx = mat_matrix.tocoo()
+        for i, j, v in zip(cx.row, cx.col, cx.data):
+            G.add_edge(i, j, weight=v)
+    else:
+        raise Exception("Dense matrices not yet supported.")
+
+    if not directed:
+        G = G.to_undirected()
+
+    t1 = time()
+    print('Graph loaded in {}s'.format(t1 - t0))
+
+    return G, x, y
+
+
+def load_citeseer_cora(citesFile='data/citeseer.cites', contentFile='data/citeseer.content', directed=True):
+    """
+    Load one of two citation networks available, CiteSeer and Cora
+    :param citesFile: the network file (edgelist)
+    :param contentFile: the user feature file
+    :param directed: whether the graph should be directed
+    :return: the graph, x feature data, y labels
+    """
+    if not citesFile.lower().endswith('.cites') or not contentFile.lower().endswith('.content'):
+        raise Exception('Wrong file type is given. First file should be *.cites and second *.content')
+
+    t0 = time()
+    G = nx.read_edgelist(citesFile, create_using=nx.DiGraph())
+    df = pd.read_csv(contentFile, sep="\t")
+    x = df.iloc[:, :-1] # drop last column
+    y = df.iloc[:, -1] # labels are stored in df's last column
+
+    if not directed:
+        G = G.to_undirected()
+
+    t1 = time()
+    print('Graph loaded in {}s'.format(t1 - t0))
+
+    return G, x, y
+
+
+def load_facebook(file, directory, directed=False):
+    """
+   Load graph from facebook dataset. Column 53 - "education type" - is used as user label.
+   :param file: the edge list facebook file
+   :param directory: the folder which contains the files that sum up the user features
+   :param directed: whether the graph should be directed
+   :return: the graph, x feature data, y labels
+   """
+
+    G = load_edgelist(file, directed=directed)
+
+    users = list()
+    for filename in os.listdir(directory):
+        if filename.endswith('.feat') or filename.endswith('.egofeat'):
+            with open(directory + '/' + filename) as f:
+                df = pd.read_csv(f, index_col=None, header=None, delimiter=' ')
+                users.append(df)
+
+    user_df = pd.concat(users, axis=0, ignore_index=True, names=[str(item) for item in np.arange(577)])
+    user_df = user_df.drop_duplicates(subset=[0]) # keep unique users
+
+    x = user_df.drop(54, axis=1)
+    y = user_df[54]
+
+    return G, x, y
+
+
+G = load_facebook('data/facebook_combined.txt', 'data/facebook')
