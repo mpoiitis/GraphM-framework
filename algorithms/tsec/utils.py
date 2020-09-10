9ef62e75b61a1605971aba8b0346707a6f7023e6
mpoiitis <marinospoet@gmail.com>
2020-09-10 16:02:28 +0300

    Add utility functions to preprocess matrices before GCN training

---
+++ algorithms/tsec/utils.py
@@ -0,0 +1,49 @@
+import numpy as np
+from scipy.sparse.linalg.eigen.arpack import eigsh
+
+
+def preprocess_features(features):
+    """Row-normalize feature matrix"""
+    rowsum = np.array(features.sum(1))
+    r_inv = np.power(rowsum, -1).flatten()
+    r_inv[np.isinf(r_inv)] = 0.
+    r_mat_inv = np.diag(r_inv)
+    features = r_mat_inv.dot(features)
+    return features
+
+
+def normalize_adj(adj):
+    """Symmetrically normalize adjacency matrix."""
+    rowsum = np.array(adj.sum(1))
+    d_inv_sqrt = np.power(rowsum, -0.5).flatten()
+    d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
+    d_mat_inv_sqrt = np.diag(d_inv_sqrt)
+    return adj.dot(d_mat_inv_sqrt).transpose().dot(d_mat_inv_sqrt)
+
+
+def preprocess_adj(adj):
+    """Normalize adjacency with added self loops"""
+    adj_normalized = normalize_adj(adj + np.eye(adj.shape[0]))
+    return adj_normalized
+
+
+def chebyshev_polynomials(adj, k):
+    """Calculate Chebyshev polynomials up to order k. Return a list of sparse matrices (tuple representation)."""
+    print("Calculating Chebyshev polynomials up to order {}...".format(k))
+
+    adj_normalized = normalize_adj(adj)
+    laplacian = np.eye(adj.shape[0]) - adj_normalized
+    largest_eigval, _ = eigsh(laplacian, 1, which='LM')
+    scaled_laplacian = (2. / largest_eigval[0]) * laplacian - np.eye(adj.shape[0])
+
+    t_k = list()
+    t_k.append(np.eye(adj.shape[0]))
+    t_k.append(scaled_laplacian)
+
+    def chebyshev_recurrence(t_k_minus_one, t_k_minus_two, scaled_lap):
+        return 2 * scaled_lap.dot(t_k_minus_one) - t_k_minus_two
+
+    for i in range(2, k+1):
+        t_k.append(chebyshev_recurrence(t_k[-1], t_k[-2], scaled_laplacian))
+
+    return t_k
